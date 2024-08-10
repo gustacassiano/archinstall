db0e1f8f36477febbe7c096cfbec9238f2b3e20d
cameiras <63693204+gustacassiano@users.noreply.github.com>
2024-08-10 19:58:07 +0000

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,5 +1,4 @@
-#############################SCRIPT DE INSTALAÇÃO ARCHLINUX ####################################
-############### PARTE 1
+#!/bin/bash
 
 printf '\033c'
 echo "
@@ -20,158 +19,146 @@ echo "
                                                                                           
 
 "
-echo "Bem-vindo ao instalador do Arch Linux por Gustavo Cameiras"
+# Configurações iniciais
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 pacman --noconfirm -Sy archlinux-keyring
 loadkeys abnt-2
 timedatectl set-ntp true
-lsblk
 reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-mkdir /mnt &>/dev/null # Hiding error message if any
 
 
-echo -ne "
--------------------------------------------------------------------------
-		         Pré-requisitos
--------------------------------------------------------------------------
-"
+# Garantindo que o pacote dialog está instalado
+if ! command -v dialog &> /dev/null; then
+    echo "O pacote 'dialog' não está instalado. Instalando..."
+    pacman --noconfirm -Sy dialog
+fi
 
+# Limpa a tela e exibe a tela inicial
+printf '\033c'
+dialog --title "Bem-vindo" --msgbox "Bem-vindo ao instalador do Arch Linux por Gustavo Cameiras" 10 50
 
+## Coleta de informações usando dialog
 hostname=$(dialog --stdout --inputbox "Insira seu hostname" 0 0) || exit 1
 : ${hostname:?"O hostname não pode estar vazio"}
+
+username=$(dialog --stdout --inputbox "Insira seu nome de usuário:" 0 0) || exit 1
+: ${username:?"O nome de usuário não pode estar vazio"}
 
 password=$(dialog --stdout --passwordbox "Insira sua senha:" 0 0) || exit 1
 : ${password:?"Campo de senha não pode estar vazio"}
 
+password_confirm=$(dialog --stdout --passwordbox "Confirme sua senha:" 0 0) || exit 1
+if [ "$password" != "$password_confirm" ]; then
+    dialog --msgbox "As senhas não coincidem. Tente novamente." 0 0
+    exit 1
+fi
+
+# Seleção do disco de instalação
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
-device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
+device=$(dialog --stdout --menu "Selecione o disco de instalação" 0 0 0 ${devicelist}) || exit 1
+
+# Escolha do sistema de boot
+boot_choice=$(dialog --stdout --menu "Escolha o sistema de boot:" 0 0 0 \
+    1 "GRUB" \
+    2 "Systemd-boot") || exit 1
+
+# Escolha do tipo de firmware
+firmware_choice=$(dialog --stdout --menu "Escolha o tipo de firmware:" 0 0 0 \
+    1 "EFI" \
+    2 "BIOS") || exit 1
+
+# Obtém o tamanho da RAM disponível
+ram_size=$(free --mebi | awk '/Mem:/ { print $2 }') # Tamanho em MiB
 
 
-
+# Particionamento
 echo -ne "
 -------------------------------------------------------------------------
 		         Formatando Disco                    
 -------------------------------------------------------------------------
 "
-
-
-
-
-
-echo -ne "
--------------------------------------------------------------------------
-                     Criando sistema de arquivos
--------------------------------------------------------------------------
-"
-
-
-
-
-
-echo -ne "
--------------------------------------------------------------------------
-			    Arch Install 
--------------------------------------------------------------------------
-"
-pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
-echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
-
-genfstab -L /mnt >> /mnt/etc/fstab
-echo " 
-  Gerado o arquivo em /etc/fstab:
-"
-cat /mnt/etc/fstab
-echo -ne "
--------------------------------------------------------------------------
-                       GRUB BIOS e Bootloader 
--------------------------------------------------------------------------
-"
-if [[ ! -d "/sys/firmware/efi" ]]; then
-    grub-install --boot-directory=/mnt/boot ${DISK}
+if [ "$firmware_choice" -eq 1 ]; then
+    # Partição EFI (mínimo 1GiB)
+    parted $device mklabel gpt
+    parted $device mkpart primary fat32 1MiB 1GiB
+    parted $device set 1 esp on
+    boot_partition="${device}1"
 else
-    pacstrap /mnt efibootmgr --noconfirm --needed
+    # Partição BIOS
+    parted $device mklabel msdos
+    parted $device mkpart primary ext4 1MiB 1GiB
+    boot_partition="${device}1"
 fi
-echo -ne "
--------------------------------------------------------------------------
-	   Checando de se o sistema tem menos de 8G de RAM
--------------------------------------------------------------------------
-"
-TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
-if [[  $TOTAL_MEM -lt 8000000 ]]; then
-    mkdir -p /mnt/opt/swap 
-    chattr +C /mnt/opt/swap 
-    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
-    chmod 600 /mnt/opt/swap/swapfile 
-    chown root /mnt/opt/swap/swapfile
-    mkswap /mnt/opt/swap/swapfile
-    swapon /mnt/opt/swap/swapfile
-    # A linha abaxixo é escrita em /mnt/ mas não contém /mnt jpa que é apenas / pro sistema.
-    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab 
+
+# Swap e root
+parted $device mkpart primary linux-swap 1GiB $((1 + ram_size))MiB
+swap_partition="${device}2"
+parted $device mkpart primary ext4 $((1 + ram_size))MiB 100%
+root_partition="${device}3"
+
+# Formatação
+mkfs.ext4 $root_partition
+mkswap $swap_partition
+swapon $swap_partition
+
+if [ "$firmware_choice" -eq 1 ]; then
+    mkfs.fat -F32 $boot_partition
+else
+    mkfs.ext4 $boot_partition
 fi
-echo -ne "
--------------------------------------------------------------------------
-              Sistema pronto para Arch Install parte 2
--------------------------------------------------------------------------
-"
 
+# Montagem
+mount $root_partition /mnt
+mkdir /mnt/boot
+mount $boot_partition /mnt/boot
 
+# Instalação dos pacotes base
+pacstrap /mnt base linux linux-firmware
 
-sed '1,/^#part2$/d' `basename $0` > /mnt/arch_install2.sh
-chmod +x /mnt/arch_install2.sh
-arch-chroot /mnt ./arch_install2.sh
-exit
+# Configuração do sistema
+genfstab -U /mnt >> /mnt/etc/fstab
 
-######### PARTE 2
-
-printf '\033c'
-pacman -S --noconfirm sed
-sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+arch-chroot /mnt /bin/bash <<EOF
+# Configurações básicas
+ln -sf /usr/share/zoneinfo/$(curl -s http://ip-api.com/line?fields=timezone) /etc/localtime
 hwclock --systohc
-echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
-echo "KEYMAP=br" > /etc/vconsole.conf
-echo "Hostname: "
-read hostname
-echo $hostname > /etc/hostname
-echo "127.0.0.1       localhost" >> /etc/hosts
-echo "::1             localhost" >> /etc/hosts
-echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
-mkinitcpio -P
-passwd
-pacman --noconfirm -S grub efibootmgr os-prober
-echo "Informe a partição EFI: " 
-read efipartition
-mkdir /boot/efi
-mount $efipartition /boot/efi 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-sed -i 's/quiet/pci=noaer/g' /etc/default/grub
-sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop \
-     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome \
-     mpv ffmpeg imagemagick  \
-     fzf man-db xclip maim \
-     zip unzip unrar p7zip xdotool papirus-icon-theme brightnessctl  \
-     git pipewire pipewire-pulse \
-     vim nvim arc-gtk-theme qutebrowser fish \
-     dunst plymouth \
-     pamixer mpd ncmpcpp networkmanager \
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "$hostname" > /etc/hostname
 
-     systemctl enable networkManager
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
 
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo " Informe seu username: "
-read username
-useradd -m -G wheel -s /bin/zsh $username
-passwd $username
-echo "Pre-Installation Finish Reboot now"
-# ai3_path=/home/$username/arch_install3.sh
-# sed '1,/^#part3$/d' arch_install2.sh > $ai3_path
-# chown $username:$username $ai3_path
-# chmod +x $ai3_path
-# su -c $ai3_path -s /bin/sh $username
+echo "Criando o usuário..."
+useradd -m $username
+echo "$username:$password" | chpasswd
+
+echo "Instalando e configurando o sistema de boot..."
+if [ "$boot_choice" -eq 1 ]; then
+    pacman -S grub --noconfirm
+    if [ "$firmware_choice" -eq 1 ]; then
+        pacman -S efibootmgr --noconfirm
+        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+    else
+        grub-install --target=i386-pc $device
+    fi
+    grub-mkconfig -o /boot/grub/grub.cfg
+else
+    pacman -S systemd-boot --noconfirm
+    bootctl install
+    echo "title Arch Linux" > /boot/loader/entries/arch.conf
+    echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+    echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+    echo "options root=PARTUUID=$(blkid -s PARTUUID -o value $root_partition) rw" >> /boot/loader/entries/arch.conf
+fi
+
 exit
+EOF
+
+echo "Instalação concluída! Desmonte as partições e reinicie o sistema."
+
+# Desmontagem e reboot
+umount -R /mnt
+swapoff -a
+reboot
